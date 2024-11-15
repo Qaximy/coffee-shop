a3fc5075c52072ab44e83b151098efa72431a86f
Nazira batool <qaximy1214@gmail.com>
2024-11-15 11:24:02 +0500

    First commit for coffee shop website

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust if needed
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brightColor: "#AB6B2E",
+        backgroundColor: "#FDE9CC",
+      },
+    },
+  },
+  plugins: [],
+};
